b6fc2fee0ebafe839676a53d54ffb977c6c4feb2
faith nkatha <faithnkatha160@gmail.com>
2024-03-28 06:26:37 +0300

    lets commit

---
+++ Asstwo.dart
@@ -0,0 +1,36 @@
+int addTwo(int num1,int num2){
+  int sum = num1 +num2;
+  return sum;
+}
+num subtractTwo(num num1 ,num num2){
+  num subResult =num1-num2;
+  return subResult;
+}
+num multiplyTwo(num num1 ,num num2){
+  num mutplyResult =num1*num2;
+  return mutplyResult;
+}
+
+double divideTwo(double num1, double num2){
+   double divideResult = num1/num2;
+   return divideResult;
+}
+int stringLength( String name){
+  int namelength = name.length;
+  return namelength;
+
+}
+int getFirstElement(List mygrades){
+int element = mygrades[0];
+return element;
+
+}
+void main(){
+  print(getFirstElement([46,78,90,60]));
+  print(stringLength("John Arthur"));
+  print(divideTwo(28, 6));
+ print(multiplyTwo(25, 5));
+ print(subtractTwo(30, 5));
+  print(addTwo(30, 5));
+}
+
---
+++ fuctions.dart
@@ -0,0 +1,42 @@
+// parameter and return type
+int add(int a, int b) {
+  var total;
+  total = a + b;
+  return total;
+}
+
+// parameter and no return type
+void mul(int a, int b) {
+  var total;
+  total = a * b;
+  print("Multiplication is : $total");
+}
+
+// no parameter and return type
+String greet() {
+  String greet = "Welcome";
+  return greet;
+}
+
+// no parameter and no return type
+void greetings() {
+  print("Hello World!!!");
+
+}
+void anonymousFunction() {
+  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+}
+
+void main() {
+  var total = add(2, 3);
+  print("Total sum: $total");
+  mul(2, 3);
+  var greeting = greet();
+  print("Greeting: $greeting");
+  greetings();
+  anonymousFunction();
+}
