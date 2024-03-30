d04b4bf6e1a975baa370db0411da0a6b13d3519f
faith nkatha <faithnkatha160@gmail.com>
2024-03-30 14:02:34 +0300

    flutter

--- Assone.dart
+++ Assone.dart
@@ -14,20 +14,4 @@ print( "my unit grade is $mygrade");
 print( "my age and name of students : $ageStudent");
 
 }
-=======
-void main(){
-String name = "Faith";
-int age = 22;
-double gp = 3.7;
-num grade = 44;
-List <int> mygrade =[70,40,80,90];
-Map <String, int> ageStudent = {"Faith":22,"Paul":20,"Ben":23};
-print( "my name is $name");
-print( "my age is $age");
-print( "my gp  is $gp");
-print( "my grade is $grade");
-print( "my unit grade is $mygrade");
-print( "my age and name of students : $ageStudent");
 
-}
->>>>>>> 2b762f642fd1458b26d111951b34dde29d030073
--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 void main(){
   print("Hello World");
   // declaring variable
@@ -28,39 +28,4 @@ print(message);
 //Boolean
 bool isAsleep = true;
 print(isAsleep);
-
-
-=======
-void main(){
-  print("Hello World");
-  // declaring variable
-  String name = "john";
-  num age = 17;
-  num height = 5.7;
-  bool isHappy = true;
-  print("Name is $name");
-   print("Name is $height");
-    print("Name is $age");
-     print("Name is $isHappy");
-     // dart data types are Numbers,Strings,Boolean,Lists,Maps,Runes,Null
-  //numbers
-  int num1 =20;
-  double num2 = 20.134;
-  num num3 = 40;
-  num num4 = 40.1;
-  num sum = num1 + num2 + num3 + num4;
-  print("Number one is $num1");
-  print("Number two $num2");
-  print("Number three $num3");
-  print("Number four $num4");
-  print("sum of Numbers is $sum");
-  //String
-String message = "I love dart";
-print(message);
-//Boolean
-bool isAsleep = true;
-print(isAsleep);
-
-
->>>>>>> 2b762f642fd1458b26d111951b34dde29d030073
 }
